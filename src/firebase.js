f53d25e55e168da67382e61be191a4f50e102f0a
Luis Arista <larista93@hotmail.com>
2022-06-21 18:48:30 -0500

    agregarndo Firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import env from 'react-dotenv';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBugBkKkPB7WshD86KXWF0QncmUFJg2BMI",
+    authDomain: "proyecto-4-restaurante-a-952ae.firebaseapp.com",
+    projectId: "proyecto-4-restaurante-a-952ae",
+    storageBucket: "proyecto-4-restaurante-a-952ae.appspot.com",
+    messagingSenderId: "1046938296867",
+    appId: "1:1046938296867:web:86c5cde31716a0c3634796"
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig)
+  const db = getFirestore( firebaseApp );
+
+  export { db }
